97d8ff011d43938138af5dc2de343f6b001b9454
Tahi-Tua <tetaurutahitua0111@gmail.com>
2026-05-12 17:19:23 +0200

    Create hero-10.js for Leviathan character
    Added hero 'Leviathan' with tips for mastery.

---
+++ data/heroes/hero-10.js
@@ -0,0 +1,21 @@
+module.exports = {
+  id: "hero10",
+  name: "🟩・Leviathan・✨",
+  image:
+    "https://th.bing.com/th/id/OIP.GLvpS9pGSYL2vcOs85r7NQHaEK?w=324&h=182&c=7&r=0&o=7&pid=1.7&rm=3",
+  tips: `Tips to Master Leviathan
+
+1.Use Wall-Piercing Fire: His machine gun can fire through walls for several seconds due to his decent piercing power and fire rate, allowing you to damage enemies hiding behind walls.
+
+2.Precautionary Turret Placement: Place turrets against walls to prevent enemies from flanking you and to guard your position.
+
+3.Manage Engagements: Avoid close-range encounters with specialized damage dealers like Sparkle and raven,Instead, play a defensive, camping-oriented style.
+
+4.Counter Invisible Enemies: Use the turret to reveal invisible enemies, as the weapon's fire rate makes it possible for him to keep firing for a brief period even after the enemy is gone or dead,
+
+5.Gear Selection: Prioritize gear that reduces bullet spread to maximize damage over distance, as his machine gun is inherently inaccurate.
+
+6.Peek firing: Occasionally attempt to attack enemies while peeking from wall corners, directly engaging enemies can be avoided using this method increasing possibility of survival even in tight conditions
+
+7.Team revival: cover yourself with a turret when attempting to revive a teammate,if zenith drone is used for revival instead, attempt cover the drone with a turret so enemy attacks don't destroy the drone
+};
