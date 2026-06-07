97228e95fefab08e04de5d22766f7f5f8955f5d3
Tahi-Tua <tetaurutahitua0111@gmail.com>
2026-06-07 16:59:39 +0200

    Disable forced cleanup default users

--- config/channels.js
+++ config/channels.js
@@ -77,9 +77,7 @@ module.exports = {
   // Role that triggers strict syndicate cleanup when a member leaves.
   SYNDICATE_MEMBER_ROLE_ID: process.env.SYNDICATE_MEMBER_ROLE_ID || null,
   SYNDICATE_MEMBER_ROLE_NAME: env("SYNDICATE_MEMBER_ROLE_NAME", "Syndicate member 🎮"),
-  SYNDICATE_CLEANUP_FORCE_USER_IDS: csv(process.env.SYNDICATE_CLEANUP_FORCE_USER_IDS, [
-    "768978119872086047", // sir_genix
-  ]),
+  SYNDICATE_CLEANUP_FORCE_USER_IDS: csv(process.env.SYNDICATE_CLEANUP_FORCE_USER_IDS, []),
   SYNDICATE_CLEANUP_KEEP_ROLE_NAMES: csv(process.env.SYNDICATE_CLEANUP_KEEP_ROLE_NAMES, [
     "Unverified",
     "Guest",
